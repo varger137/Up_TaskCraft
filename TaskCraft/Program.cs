399d520089b65df3519b1a8e78ac1a4f174ed775
Varger137 <vadimos.shtaygeros@gmail.com>
2025-03-04 12:58:22 +0500

    добавление бд в program.cs

--- TaskCraft/Program.cs
+++ TaskCraft/Program.cs
@@ -1,6 +1,30 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+using Microsoft.EntityFrameworkCore;
+using TaskCraft.DataBase;
+
+using TaskCraft.Repositories;
+
+
+
+#region Builder
+var builder = WebApplication.CreateBuilder();
+
+
+builder.Services.AddAutoMapper(typeof(Program));
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddScoped<UserRepository>();
+builder.Services.AddScoped<ProjectRepository>();
+builder.Services.AddScoped<ChatRepository>();
+builder.Services.AddScoped<MessageRepository>();
+builder.Services.AddScoped<TaskRepository>();
+#endregion
+
+#region Apps
+var app = builder.Build();
+#endregion
+
 
 app.Run();
